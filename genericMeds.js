d00bdf357fc786741e64cdb02cce4387a78fe53b
Rishabh Jain <risjai@microsoft.com>
2019-03-29 00:03:36 +0530

    Curate data

--- genericMeds.js
+++ genericMeds.js
@@ -1,162 +1,259 @@
-exports.default = [{
-    id: '100001',
-    name: 'Aceclofenac + Paracetamol (100 mg + 325 mg) Tablets',
-    activeIngredients: [{
-        id: 'Aceclofenac',
-        weight: '100mg'
-      },
-      {
-        id: 'Paracetamol',
-        weight: '325mg'
-      }
+exports.default = [
+  {
+    //   id: '100001',
+    //   name: 'Aceclofenac + Paracetamol (100 mg + 325 mg) Tablets',
+    //   activeIngredients: [{
+    //       id: 'Aceclofenac',
+    //       weight: '100mg'
+    //     },
+    //     {
+    //       id: 'Paracetamol',
+    //       weight: '325mg'
+    //     }
+    //   ],
+    //   unitSize: "10's",
+    //   price: '5.7'
+    // },
+    id: "100002",
+    name: "Aceclofenac 100 mg Tablets",
+    unitSize: "10's",
+    price: "3.84",
+    uses: [
+      "Used to treat symptoms like swelling, pain, and stiffness of joints associated with Rheumatoid Arthritis.",
+      "Used to treat symptoms like tender and painful joints associated with Osteoarthritis.",
+      "Used to treat symptoms like stiffness and pain associated with Ankylosing Spondylitis."
     ],
-    unitSize: "10's",
-    price: '5.7'
+    activeIngredients: [
+      {
+        id: "Aceclofenac",
+        weight: "100 mg"
+      }
+    ]
   },
+  // {
+  //   id: "100003",
+  //   name: "Diclofenac 50 mg+ paracetamol 325 mg+ chlorzoxazone 500 mg tablets",
+  //   unitSize: "10's",
+  //   price: "12.4"
+  // },
   {
-    id: '100002',
-    name: 'Aceclofenac 100 mg Tablets',
-    unitSize: "10's",
-    price: '3.84'
-  },
-  {
-    id: '100003',
-    name: 'Diclofenac 50 mg+ paracetamol 325 mg+ chlorzoxazone 500 mg tablets',
-    unitSize: "10's",
-    price: '12.4'
-  },
-  {
-    id: '100004',
-    name: 'Diclofenac Gel ( Diclofenec Diethylamine 1.16 % w/w)',
-    unitSize: '15 g',
-    price: '8.22'
-  },
-  {
-    id: '100005',
-    name: 'Diclofenac Sodium + Serratiopeptidase (50 mg + 10 mg) Tab',
-    unitSize: "10's",
-    price: '7.02'
-  },
-  {
-    id: '100006',
-    name: 'Diclofenac Sodium (SR) Tablets 100 mg',
-    unitSize: "10's",
-    price: '6.12'
-  },
-  {
-    id: '100007',
-    name: 'Diclofenac Sodium 50 mg Tab',
-    unitSize: "10's",
-    price: '2.3'
-  },
-  {
-    id: '100008',
-    name: 'Etoricoxilb 120mg Tab',
-    unitSize: "10's",
-    price: '33.96'
-  },
-  {
-    id: '100009',
-    name: 'Etoricoxilb 90mg Tab',
-    unitSize: "10's",
-    price: '25.99'
-  },
-  {
-    id: '100010',
-    name: 'Ibuprofen film coated Tablets 200mg',
-    unitSize: "10's",
-    price: '1.75'
-  },
-  {
-    id: '100011',
-    name: 'Ibuprofen Tablets 400mg',
-    unitSize: "15's",
-    price: '4.88'
-  },
-  {
-    id: '100012',
-    name: 'Nimesulide 100 mg Tab',
-    unitSize: "10's",
-    price: '2.52',
+    id: "100004",
+    name: "Diclofenac Gel ( Diclofenec Diethylamine 1.16 % w/w)",
+    unitSize: "15 g",
+    price: "8.22",
     uses: [
-      'Acute Pain',
-      'Osteoarthritic Pain',
-      'Primary Dysmenorrhea',
+      "Used to treat symptoms like swelling, pain, and stiffness of joints associated with Rheumatoid Arthritis.",
+      "Used to treat symptoms like tender and painful joints associated with Osteoarthritis.",
+      "Used to treat symptoms like stiffness and pain associated with Ankylosing Spondylitis.",
+      "Used to relieve excessive pain and cramps during menstruation.",
+      "Used to relieve pain of sprains, strains, sports injuries etc.",
+      "Used to relieve acute pain in migraine.",
+      "Used to relieve swelling and pain in joints caused by the inflammation of the bursae.",
+      "Used to relieve pain and swelling associated with the tissue connecting the muscle and bones."
+    ],
+    activeIngredients: [
+      {
+        id: "Diclofenac",
+        weight: "15 g"
+      }
+    ]
+  },
+  // {
+  //   id: "100005",
+  //   name: "Diclofenac Sodium + Serratiopeptidase (50 mg + 10 mg) Tab",
+  //   unitSize: "10's",
+  //   price: "7.02"
+  // },
+  // {
+  //   id: "100006",
+  //   name: "Diclofenac Sodium (SR) Tablets 100 mg",
+  //   unitSize: "10's",
+  //   price: "6.12"
+  // },
+  {
+    id: "100007",
+    name: "Diclofenac Sodium 50 mg Tab",
+    unitSize: "10's",
+    price: "2.3",
+    uses: [
+      "Used to treat symptoms like swelling, pain, and stiffness of joints associated with Rheumatoid Arthritis.",
+      "Used to treat symptoms like tender and painful joints associated with Osteoarthritis.",
+      "Used to treat symptoms like stiffness and pain associated with Ankylosing Spondylitis.",
+      "Used to relieve excessive pain and cramps during menstruation.",
+      "Used to relieve pain of sprains, strains, sports injuries etc.",
+      "Used to relieve acute pain in migraine.",
+      "Used to relieve swelling and pain in joints caused by the inflammation of the bursae.",
+      "Used to relieve pain and swelling associated with the tissue connecting the muscle and bones."
+    ],
+    activeIngredients: [
+      {
+        id: "Diclofenac",
+        weight: "50 mg"
+      }
     ]
   },
   {
-    id: '100013',
-    name: 'Paracetamol + Diclofenac Sodium (325 mg + 50 mg) Tab',
+    id: "100008",
+    name: "Etoricoxilb 120mg Tab",
     unitSize: "10's",
-    price: '4.34'
-  },
-  {
-    id: '100014',
-    name: 'Serratiopeptidase Tablets 10 mg',
-    unitSize: "10's",
-    price: '5.97'
-  },
-  {
-    id: '100015',
-    name: 'Tramadol Hydrochloride 50 mg/ml Inj.',
-    unitSize: '2ml',
-    price: '3.15'
-  },
-  {
-    id: '100016',
-    name: 'Tramadol Hydrochloride 50 mg/ml Inj.',
-    unitSize: '1ml',
-    price: '3.37'
-  },
-  {
-    id: '100017',
-    name: 'Tramadol Hydrochloride 50 mg Tab',
-    unitSize: "10's",
-    price: '4.38'
-  },
-  {
-    id: '100022',
-    name: 'Tramadol Hydrochloride 100 mg Tab',
-    unitSize: '2ml',
-    price: '4.38',
-    description: 'Tramazac 100 MG Injection is a synthetic painkiller belonging to a group of medicines known as Opioid Analgesics. It is used to relieve moderate to severe pain. It is also used in relieving acute pain after surgery.'
-  },
-  {
-    id: '100018',
-    name: 'Allopurinol 100 mg Tabs',
-    unitSize: "10's",
-    price: '6.95'
-  },
-  {
-    id: '100019',
-    name: 'Tranexamic Acid 500 mg+ Mefenamic Acid 250 mg Tablets',
-    unitSize: "10's",
-    price: '78.09'
-  },
-  {
-    id: '100020',
-    name: 'Paracetamol 325 mg+ Tramadol 37.5 mg Tablets',
-    unitSize: "10's",
-    price: '7.41'
-  },
-  {
-    id: '100021',
-    name: 'Paracetamol Tablets 650 mg',
-    unitSize: "15's",
-    price: '8.03'
-  },
-  {
-    id: '100023',
-    name: 'Tramadol Hcl 100 mg Inj',
-    ingredients: [{
-      name: 'Tramadol',
-      potency: '100mg'
-    }],
-    unitSize: '2ml',
-    price: '8.03',
+    price: "33.96",
     uses: [
-      'Moderate to severe pain',
-      'Chronic pain'
+      "Used for relieving pain and swelling of joints associated with Osteoarthritis.",
+      "Used for relieving swelling, stiffness, and pain of joints caused due to Rheumatoid arthritis.",
+      "Used for the symptomatic treatment of Ankylosing spondylitis. This disease causes swelling of the spine and large joints.",
+      "Used to relieve pain and swelling of joints associated with gout."
+    ],
+    activeIngredients: [
+      {
+        id: "Etoricoxib",
+        weight: "120 mg"
+      }
     ]
-  }
+  },
+  {
+    id: "100009",
+    name: "Etoricoxilb 90mg Tab",
+    unitSize: "10's",
+    price: "25.99",
+    uses: [
+      "Used for relieving pain and swelling of joints associated with Osteoarthritis.",
+      "Used for relieving swelling, stiffness, and pain of joints caused due to Rheumatoid arthritis.",
+      "Used for the symptomatic treatment of Ankylosing spondylitis. This disease causes swelling of the spine and large joints.",
+      "Used to relieve pain and swelling of joints associated with gout."
+    ],
+    activeIngredients: [
+      {
+        id: "Etoricoxib",
+        weight: "90 mg"
+      }
+    ]
+  },
+  {
+    id: "100010",
+    name: "Ibuprofen film coated Tablets 200mg",
+    unitSize: "10's",
+    price: "1.75",
+    uses: [
+      "Used to relieve excessive pain and cramps during menstruation.",
+      "Used to treat symptoms like tender and painful joints associated with Osteoarthritis. ",
+      "Used to treat symptoms like swelling, pain, and stiffness of joints associated with Rheumatoid Arthritis.",
+      "Used to treat headache, back pain, and fever."
+    ],
+    activeIngredients: [
+      {
+        id: "Ibuprofen",
+        weight: "200 mg"
+      }
+    ]
+  },
+  {
+    id: "100011",
+    name: "Ibuprofen Tablets 400mg",
+    unitSize: "15's",
+    price: "4.88",
+    uses: [
+      "Used to relieve excessive pain and cramps during menstruation.",
+      "Used to treat symptoms like tender and painful joints associated with Osteoarthritis. ",
+      "Used to treat symptoms like swelling, pain, and stiffness of joints associated with Rheumatoid Arthritis.",
+      "Used to treat headache, back pain, and fever."
+    ],
+    activeIngredients: [
+      {
+        id: "Ibuprofen",
+        weight: "400 mg"
+      }
+    ]
+  },
+  // {
+  //   id: "100012",
+  //   name: "Nimesulide 100 mg Tab",
+  //   unitSize: "10's",
+  //   price: "2.52",
+  //   uses: ["Acute Pain", "Osteoarthritic Pain", "Primary Dysmenorrhea"]
+  // },
+  // {
+  //   id: "100013",
+  //   name: "Paracetamol + Diclofenac Sodium (325 mg + 50 mg) Tab",
+  //   unitSize: "10's",
+  //   price: "4.34"
+  // },
+  {
+    id: "100014",
+    name: "Serratiopeptidase Tablets 10 mg",
+    unitSize: "10's",
+    price: "5.97",
+    uses: [
+      "Used to relieve swelling and pain associated with conditions like trauma, arthritis etc.",
+      "Used along with other medications to reduce fluid accumulation (edema) in the body."
+    ],
+    activeIngredients: [
+      {
+        id: "Serratiopeptidase",
+        weight: "10 mg"
+      }
+    ]
+  },
+  // {
+  //   id: "100015",
+  //   name: "Tramadol Hydrochloride 50 mg/ml Inj.",
+  //   unitSize: "2ml",
+  //   price: "3.15"
+  // },
+  // {
+  //   id: "100016",
+  //   name: "Tramadol Hydrochloride 50 mg/ml Inj.",
+  //   unitSize: "1ml",
+  //   price: "3.37"
+  // },
+  // {
+  //   id: "100017",
+  //   name: "Tramadol Hydrochloride 50 mg Tab",
+  //   unitSize: "10's",
+  //   price: "4.38"
+  // },
+  // {
+  //   id: "100022",
+  //   name: "Tramadol Hydrochloride 100 mg Tab",
+  //   unitSize: "2ml",
+  //   price: "4.38",
+  //   description:
+  //     "Tramazac 100 MG Injection is a synthetic painkiller belonging to a group of medicines known as Opioid Analgesics. It is used to relieve moderate to severe pain. It is also used in relieving acute pain after surgery."
+  // },
+  // {
+  //   id: "100018",
+  //   name: "Allopurinol 100 mg Tabs",
+  //   unitSize: "10's",
+  //   price: "6.95"
+  // },
+  // {
+  //   id: "100019",
+  //   name: "Tranexamic Acid 500 mg+ Mefenamic Acid 250 mg Tablets",
+  //   unitSize: "10's",
+  //   price: "78.09"
+  // },
+  // {
+  //   id: "100020",
+  //   name: "Paracetamol 325 mg+ Tramadol 37.5 mg Tablets",
+  //   unitSize: "10's",
+  //   price: "7.41"
+  // },
+  // {
+  //   id: "100021",
+  //   name: "Paracetamol Tablets 650 mg",
+  //   unitSize: "15's",
+  //   price: "8.03"
+  // },
+  // {
+  //   id: "100023",
+  //   name: "Tramadol Hcl 100 mg Inj",
+  //   ingredients: [
+  //     {
+  //       name: "Tramadol",
+  //       potency: "100mg"
+  //     }
+  //   ],
+  //   unitSize: "2ml",
+  //   price: "8.03",
+  //   uses: ["Moderate to severe pain", "Chronic pain"]
+  // }
 ];
